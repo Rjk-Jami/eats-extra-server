585e9155a73d0742b2d01f6920062679f180ad0a
Rjk-Jami <rjkhan.1221@gmail.com>
2023-05-03 18:22:31 +0600

    add  recipes

---
+++ data/demo.jsx
@@ -0,0 +1,35 @@
+1. Jamie Oliver:
+- Chicken in Milk
+- Pesto Pasta
+- 30-Minute Shepherd's Pie
+- Veggie Chili
+
+2. Nigella Lawson:
+- Chocolate Fudge Cake
+- Lemon Drizzle Cake
+- Spaghetti Carbonara
+- Sticky Toffee Pudding
+
+3. Gordon Ramsay:
+- Beef Wellington
+- Sticky Toffee Pudding
+- Shepherd's Pie
+- Scrambled Eggs
+
+4. Heston Blumenthal:
+- Triple-Cooked Chips
+- Meat Fruit
+- Sound of the Sea
+- Bacon and Egg Ice Cream
+
+5. Mary Berry:
+- Lemon Drizzle Cake
+- Victoria Sponge Cake
+- Chocolate Fudge Cake
+- Shepherd's Pie
+
+6. Tom Kerridge:
+- Proper Fish and Chips
+- Slow-Cooked Beef Brisket
+- Chicken Kiev
+- Venison Casserole 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const chefs = require('./data/chefs.json')
+const recipes = require ("./data/recipes.json")
 const cors = require('cors')
 app.use(cors())
 
@@ -22,6 +23,25 @@ app.get('/chefs', (req, res) => {
     res.send(selectedChef);
   })
 
+//all recipes
+app.get('/recipes', (req, res) => {
+  res.send(recipes)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+//get recipes  by chef id
+app.get('/recipes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (id === 0) {
+    res.send(recipes)
+  }
+
+  else {
+    const chefRecipes = recipes.filter(n => parseInt(n.chef_id) === id)
+    res.send(chefRecipes)
+  }
 })
